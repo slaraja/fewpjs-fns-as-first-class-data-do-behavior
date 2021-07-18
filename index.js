5dbd0410e892ec4acd112b423ffd134c7a6ac500
Stephanie La Raja <stephanielaraja@gmail.com>
2021-07-18 13:19:17 -0400

    commit tree 4ac351207864e1c5f1eee0c0631342a5207e9ed8

--- index.js
+++ index.js
@@ -9,5 +9,20 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(time) {
+  let t = time.split(':')
+  let hour = parseInt(t[0]);
+
+  if (hour < 12) return "Good Morning";
+  if (hour < 17) return "Good Afternoon";
+
+  return "Good Evening";
+}
+
+function displayMessage(string) {
+  let greeting = document.getElementById('greeting');
+
+  greeting.innerText = string;
+
+}
+
